d1748b0c32ea52b8d6fa54c9a8934534b411460d
krishmanavadariya <147189912+krishmanavadariya@users.noreply.github.com>
2026-04-18 11:15:17 +0530

    Delete assigment directory

--- assigment/nodepractical/Q1.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req, res) {
-    res.write("Hello from Server");
-    res.end();
-}).listen(5000);
-
-console.log("Server running...");
--- assigment/nodepractical/Q4.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req, res) {
-    res.write("Welcome to NodeJS App");
-    res.end();
-}).listen(3000);
-
-console.log("Server running...");
